5a20fd31fedec1b8375a5d5a4c8cb7800f6f0bcd
CL <cl@lbx.dev>
2023-05-17 15:11:58 +0800

    Initial Commit

---
+++ CLFramework/CLFramework.h
@@ -0,0 +1,18 @@
+//
+//  CLFramework.h
+//  CLFramework
+//
+//  Created by LBX-CL on 2023/5/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLFramework.
+FOUNDATION_EXPORT double CLFrameworkVersionNumber;
+
+//! Project version string for CLFramework.
+FOUNDATION_EXPORT const unsigned char CLFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLFramework/PublicHeader.h>
+
+
